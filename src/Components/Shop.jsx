b3e967b8bb2eed70912af46089a51ce2c70aae9a
Wuttha <Wuttha@webmail.npru.ac.th>
2024-06-27 17:17:06 +0700

    27/06/67-รถยังเสีย

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card } from "./Card";
 
 import { restaurants } from "../data/dataRestaurant";
-const Shop = () => {
+const Shop = ({ restaurants }) => {
   
   return (
     <>
