bd1995e90e4feb955a83e9d5fcccc3b91c1f25eb
Wuttha <Wuttha@webmail.npru.ac.th>
2024-08-22 17:10:50 +0700

    added loading

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,22 @@ import UserProfile from "./UserProfile";
 import LoginButton from "./LoginButton";
 import RegisterButton from "./RegisterButton";
 import { useAuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { user } = useAuthContext();
+const menus = {
+  ROLES_ADMIN: [
+    { name: "Add restaurant", link: "/add" },
+    { name: "Search", link: "/search" },
+    { name: "Dashboard", link: "/dashboard" },
+  ],
+  ROLES_USER: [{ name: "Search", link: "/search" }],
+  ROLES_MODERATOR: [
+    { name: "Add restaurant", link: "/add" },
+    { name: "Search", link: "/" },
+  ],
+};
   
   return (
     <div>
@@ -22,8 +35,6 @@ const Navbar = () => {
             </a>
           </div>
 
-          
-
           {user ? (
             <UserProfile />
           ) : (
@@ -33,7 +44,22 @@ const Navbar = () => {
             </div>
           )}
         </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
+          {user &&
+            menus[user.roles[0]].map((menuItem) => (
+              <li key={menuItem.name}>
+                <a href={menuItem.link}>{menuItem.name}</a>
+              </li>
+            ))}
+        </ul>
+        <a className="btn btn-ghost text-xl">Grab Restaurant</a>
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1"></ul>
+      </div>{" "}
     </div>
   );
 };
